dd8b46f4c3612c1751a6113810a06aac7bc2b242
chris <chinyu0723@gmail.com>
2020-11-25 16:40:51 +0800

    首頁最新消息完成＋GSAP動畫

---
+++ js/sm-GSAP.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+
+  $('.news1').each(function(){
+
+    //build a scene
+  var ourScene12 = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.8,
+      reverse: true
+  })
+  .setClassToggle(this, 'fade-in') //add class to section1-pic
+  .addTo(controller);
+  });
+
+  $('.news1-pic').each(function(){
+
+    //build a scene
+  var ourScene12 = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.7,
+      reverse: true
+  })
+  .setClassToggle(this, 'fade-in-left') //add class to section1-pic
+  .addTo(controller);
+  });
+
+  $('.news1-list').each(function(){
+
+    //build a scene
+  var ourScene12 = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.7,
+      reverse: true
+  })
+  .setClassToggle(this, 'fade-in') //add class to section1-pic
+  .addTo(controller);
+  });
+
+  $('.news2').each(function(){
+
+    //build a scene
+  var ourScene12 = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.8,
+      reverse: true
+  })
+  .setClassToggle(this, 'fade-in') //add class to section1-pic
+  .addTo(controller);
+  });
+
+
+});
